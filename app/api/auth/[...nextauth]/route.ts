f94b4d2e014446cdda878bb9aa65dd1be1ef5c35
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-09-16 23:47:16 +0530

    Create route.ts

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+
+export const authOptions : NextAuthOptions = {providers: [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    }),
+  ],
+  
+  callbacks: {
+    
+
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
+
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export {handler as GET , handler as POST}
