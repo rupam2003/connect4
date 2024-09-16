1cb18ef2962490056adea3ddd01f316d78b3f248
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-09-17 00:03:38 +0530

    Update route.ts Dumb nextauth problem

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 
-export const authOptions : NextAuthOptions = {providers: [
+const authOptions : NextAuthOptions = {providers: [
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
