197f2b3b3f633ca7bedb7725985bef3e222e38b5
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-09-19 23:41:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,12 @@ const Page = async () => {
         name = {session?.user?.name as string}
         image = {session?.user?.image as string}
         />
-        <h1 className='text-3xl text-center mb-5 '>Connect, Compete, Conquer<br/> Dive into the Ultimate Connect4 Challenge!</h1>
+        <h1 className='text-2xl text-center mb-5 '>Connect, Compete, Conquer<br/> Dive into the Ultimate Connect4 Challenge!</h1>
         <div className='flex flex-col gap-4 sm:flex-row'>
-          <div className='flex justify-center rounded-xl btn bg-white py-2  text-2xl '>
+          <div className='flex justify-center rounded-xl btn bg-white py-2  text-xl '>
             <Link className='px-12'  href={"/create"}>CREATE</Link>
           </div>
-          <div className='flex justify-center  rounded-xl btn bg-white py-2 text-2xl'>
+          <div className='flex justify-center  rounded-xl btn bg-white py-2 text-xl'>
             <Link className='px-12'  href={"/join"}>JOIN</Link>
           </div>
         
