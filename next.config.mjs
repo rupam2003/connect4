09898181ebd97093b55515ef23eab60475e6d0a5
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-09-21 00:06:22 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   
+    images: {
+        unoptimized:true,
+        domains: ['lh3.googleusercontent.com',],
+    },
+};
+
+export default nextConfig;
