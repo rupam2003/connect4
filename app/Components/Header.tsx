52a4677ad5fafbfae6ba922764145ebd0badbd86
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-09-22 19:18:35 +0530

    Update Header.tsx add signout button

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,19 +1,26 @@
 "use client"
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import React from 'react'
+
 
 const Header = () => {
     const {data:session} = useSession()
 
     return (
-    <header className='bg-black m-2'>
-        <div className='rounded-xl border-[1px] border-[rgb(39, 39, 42)] flex justify-between items-center max-w-[800px] mx-auto'>
+    <header className='bg-black  m-2'>
+        <div className=' rounded-xl border-[1px] border-[rgb(39, 39, 42)] flex justify-between items-center max-w-[800px] mx-auto'>
         
         <h1 className='text-2xl ml-4 m-2'>CONNECT4</h1>
         {
             session?.user?.image
-            ?<div className='w-9 h-9 mr-3  relative '><Image className='rounded-full' src={session.user.image} fill objectFit='contain' alt={"profile"}/></div>
+            ?<div className='flex gap-3 mr-3'>
+            <div className='w-9 h-9  relative '><Image className='rounded-full' src={session.user.image} fill objectFit='contain' alt={"profile"}/></div>
+            <button onClick={()=>signOut()} className='text-base font-normal px-2 border-[1px] rounded-lg  bg-[#020617]'>Log out</button>
+            
+            </div>
+            
+            
             
             :<div className='w-9 h-9 mr-3  relative '></div>
         }
