e844ff5d66b0b10d5faddd9ad3bb1c0b823fb19c
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-11-11 15:21:05 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const Page = async () => {
         name = {session?.user?.name as string}
         image = {session?.user?.image as string}
         />
-        h1 className='text-4xl sm:text-6xl text-center font-bold mx-10 relative z-20 bg-clip-text text-transparent bg-gradient-to-r from-[#06619C] to-[#00FF95]'>Welcome to CONNECTx4 </h1>
+        <h1 className='text-4xl sm:text-6xl text-center font-bold mx-10 relative z-20 bg-clip-text text-transparent bg-gradient-to-r from-[#06619C] to-[#00FF95]'>Welcome to CONNECTx4 </h1>
         <h1 className='mb-6 text-xl sm:text-2xl text-center font-bold mx-10 relative z-20 bg-clip-text text-transparent bg-gradient-to-r from-[#06619C] to-[#00FF95]'>
           Play Online Multiplayer Connect4 Board Game
         </h1>
