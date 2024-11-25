1341fc87d8338d826a2f3330dba9f7420cbe6b3f
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-11-26 01:14:35 +0530

    Update Header.tsx border color change

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -9,14 +9,14 @@ const Header = () => {
 
     return (
     <header className='z-50 sticky top-2 w-full bg-black max-w-[800px] m-2'>
-        <div className=' rounded-xl border-[1px] border-[rgb(39, 39, 42)] mx-3 flex justify-between items-center'>
+        <div className=' rounded-xl border-[1px] border-[#212121] mx-3 flex justify-between items-center'>
         
         <h1 className='text-2xl ml-4 m-2'>CONNECTx4</h1>
         {
             session?.user?.image
             ?<div className='flex gap-3 mr-3'>
             <div className='w-9 h-9  relative '><Image className='rounded-full' src={session.user.image} fill objectFit='contain' alt={"profile"}/></div>
-            <button onClick={()=>signOut()} className='text-base font-normal px-2 border-[1px] rounded-lg  bg-[#020617]'>Log out</button>
+            <button onClick={()=>signOut()} className='text-base font-normal px-2 border-[1px]  border-[#212121] rounded-lg  bg-[#020617]'>Log out</button>
             
             </div>
             
